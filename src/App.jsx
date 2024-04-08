b84b9b67b02276b569b3d10581b3aeb8aed69926
Abiero Alvin <alvinabiero@gmail.com>
2024-04-08 14:57:25 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ThumbnailSlider from "./components/ThumbnailSlider";
+
+function App() {
+  return (
+    <div className="w-full h-screen mx-auto bg-purple-800 flex items-center justify-center">
+      <ThumbnailSlider />
+    </div>
+  );
+}
+
+export default App;
